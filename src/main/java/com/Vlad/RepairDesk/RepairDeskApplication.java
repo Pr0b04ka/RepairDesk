444458f1ce8033bf45fe94ee648ab8873672e4fe
Pr0b04ka <saenkov100@gmail.com>
2026-03-03 17:20:59 +0200

    Initial commit

---
+++ src/main/java/com/Vlad/RepairDesk/RepairDeskApplication.java
@@ -0,0 +1,13 @@
+package com.Vlad.RepairDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/Vlad/RepairDesk/RepairDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vlad.RepairDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
